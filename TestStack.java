d989f272f4b59bdc04ce89ba2c8a45c31e4e9538
ST <gowtham.st@dxc.com>
2022-05-18 15:38:45 +0530

    Added - Teststack code using Linked List

---
+++ TestStack.java
@@ -0,0 +1,18 @@
+package com.BridgeLabz_Day14Assignment;
+
+public class TestStack {
+	public static void main(String[] args)
+	{
+		Stack stack = new Stack();
+		stack.push(56);
+		stack.push(30);
+		stack.push(70);
+		stack.display();
+		stack.peak();
+
+		for (int i=0;i<4;i++){
+			stack.pop();
+		}
+	}
+
+}
